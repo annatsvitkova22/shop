7028573447ad0ab1c8ecc97bc7973f2ffccee863
annatsvitkova22 <tsvitkova.work@gmail.com>
2019-11-11 17:23:34 +0200

    defect with ts reducers-index, author-list and store

--- src/containers/author-list.tsx
+++ src/containers/author-list.tsx
@@ -9,9 +9,17 @@ import { REMOVE_AUTHOR, ADD_AUTHOR } from '../constants';
 import '../components/author.css';
 
 const BASE_PATH = 'https://localhost:443/author/';
-
+interface test {
+    id?: string,
+    name?: string,
+    isRemoved?: boolean
+}
+interface IState {
+    tests?: test[],
+    taskText?: '',
+}
 class Author extends Component {
-    constructor(props) {
+    constructor(props: IState) {
         super(props);
         this.state = ({
             tests: [],
@@ -19,15 +27,15 @@ class Author extends Component {
         });
     }
 
-    handleInputChange = ({ target: { value } }) => {
+    handleInputChange = ({ target: { value } }: any) => {
         this.setState({
             taskText: value,
         })
     }
 
     addAuthor = () => {
-        const { taskText } = this.state;
-        const { addAuthor } = this.props;
+        const { taskText }: any = this.state;
+        const { addAuthor }: any = this.props;
         const createAuthor = {
             name: taskText,
         };
@@ -39,7 +47,7 @@ class Author extends Component {
         });
     }
 
-    deleteAuthor = (id) => {
+    deleteAuthor = (id: string) => {
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
         const options = {
@@ -61,15 +69,15 @@ class Author extends Component {
         this.getAllAuthors();
     }
 
-    componentDidUpdate(prevProps) {
-        console.log(this.state.tests);
-        console.log(this.props.tasks);
-        if (this.props.tasks[1] == REMOVE_AUTHOR) {
-            this.deleteAuthor(this.props.tasks[0]);
-            this.props.tasks[1] = ADD_AUTHOR;
+    componentDidUpdate(prevProps: any) {
+        const { tasks }: any = this.props;
+
+        if (tasks[1] == REMOVE_AUTHOR) {
+            this.deleteAuthor(tasks[0]);
+            tasks[1] = ADD_AUTHOR;
             this.getAllAuthors();
         }
-        if (this.props.tasks !== prevProps.tasks) {
+        if (tasks !== prevProps.tasks) {
             this.getAllAuthors();
           
         }
@@ -89,7 +97,7 @@ class Author extends Component {
             .catch(error => error);
     }
 
-    createAuthor = (data) => {
+    createAuthor = (data: test) => {
         const json = JSON.stringify(data);
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
@@ -104,8 +112,8 @@ class Author extends Component {
     }
 
     render() {
-        const { taskText, tests } = this.state;
-        const { removeAuthor } = this.props;
+        const { taskText, tests }: any = this.state;
+        const { removeAuthor }: any = this.props;
 
         return (
             <div className="author-wrapper">
@@ -116,6 +124,6 @@ class Author extends Component {
     }
 }
 
-export default connect(({ tasks }) => ({
+export default connect(({ tasks }: any) => ({
     tasks,
 }), { addAuthor, removeAuthor, completeAuthor })(Author);
